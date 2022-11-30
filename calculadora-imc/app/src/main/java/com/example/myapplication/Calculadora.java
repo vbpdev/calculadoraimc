4bf6f55376dd741117ba250f1ac121efe48032d7
Vitor Prates <vprates2323@gmail.com>
2022-11-29 21:45:47 -0300

    calculadora pronta

---
+++ calculadora-imc/app/src/main/java/com/example/myapplication/Calculadora.java
@@ -0,0 +1,78 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Calculadora extends AppCompatActivity {
+    Button btnCalcular, btnVoltar;
+    Double imc, peso, altura;
+    EditText edtpeso, edtaltura;
+    TextView txtResultadoIMC, txtSituacao;
+    String resultado;
+    Intent i;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnVoltar = findViewById(R.id.btnTela2);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    i = new Intent(Calculadora.this, MainActivity.class);
+                    startActivity(i);
+                }
+            });
+
+        btnCalcular = findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtpeso = findViewById(R.id.edtPeso);
+                peso = Double.parseDouble(edtpeso.getText().toString());
+                edtaltura = findViewById(R.id.edtAltura);
+                altura = Double.parseDouble(edtaltura.getText().toString());
+                imc = peso/(altura*altura);
+                txtResultadoIMC = findViewById(R.id.txtResultadoIMC);
+                txtResultadoIMC.setText(Double.toString(imc));
+
+                if(imc < 17) {
+                    resultado = "Bem abaixo do peso";
+                }
+
+                else if (imc >=17 && imc < 18.5) {
+                    resultado = "Abaixo do peso";
+                }
+
+                else if (imc >=18.5 && imc < 25) {
+                    resultado = "Peso normal";
+                }
+
+                else if (imc >= 25 && imc < 30) {
+                    resultado = "Acima do peso";
+                }
+
+                else if (imc >= 30 && imc < 35) {
+                    resultado = "Obesidade Categoria I";
+                }
+
+                else if (imc >= 35 && imc < 40) {
+                    resultado = "Obesidade Categoria II";
+                }
+
+                else {
+                    resultado = "Obesidade Categoria III";
+                }
+
+                txtSituacao = findViewById(R.id.txtSituacao);
+                txtSituacao.setText(resultado);
+            }
+        });
+    }
+}
